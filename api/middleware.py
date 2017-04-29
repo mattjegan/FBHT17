55df4dd4b3e4d1a38e6945302951856a7b2e0adf
mattjegan <matthewj.egan@hotmail.com>
2017-04-29 17:14:37 +1000

    fix settings and add ExpiryMiddleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,18 +1,22 @@
 
-# from api.models import Mission
-#
-# class ExpiryMiddleware(object):
-#     """
-#     This should DEFINITELY be a celery task but I cbb setting up a celery queue on heroku right now
-#     """
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#
-#     def __call__(self, request):
-#
-#         # Expire any missions needing expiry
-#         Mission.objects.filter(expire=)
-#
-#         response = self.get_response(request)
-#
-#         return response
+from datetime import datetime
+
+from django.utils.deprecation import MiddlewareMixin
+
+from api.models import Mission
+
+class ExpiryMiddleware(MiddlewareMixin):
+    """
+    This should DEFINITELY be a celery task but I cbb setting up a celery queue on heroku right now
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        # Expire any missions needing expiry
+        Mission.objects.filter(expire__lt=datetime.now()).update(active=False)
+
+        response = self.get_response(request)
+
+        return response
